4ec3a12be9bb278a2c075f5a4696442179266044
juztinlazaro <juztinlazaro@gmail.com>
2019-11-06 18:05:17 +1100

    changes: graphql example

--- src/store/App/app.api.ts
+++ src/store/App/app.api.ts
@@ -2,6 +2,6 @@ import { API, graphqlOperation } from "aws-amplify";
 import { listNotes } from "../../graphql/queries";
 import { createNote } from "../../graphql/mutations";
 
-export const graphlAPI = async (endpoint: string, input?: any) => {
-  return await API.graphql(graphqlOperation(endpoint, input || {}));
+export const getNoteListApi = async () => {
+  return await API.graphql(graphqlOperation(listNotes));
 };
